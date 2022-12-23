d5c62ef8cf91f2fc21120da53000867dbc00c345
nuvvulauday2121 <102281701+nuvvulauday2121@users.noreply.github.com>
2022-12-23 09:41:44 +0530

    First commit
    Uploding all the files

---
+++ basic/src/com/creatiq/operetors/UnaryOperator.java
@@ -0,0 +1,14 @@
+package com.creatiq.operetors;
+
+public class UnaryOperator {
+	
+	public static void main(String[] args) {
+		int a =10;
+		
+		System.out.println(a++);
+		System.out.println(++a);
+		System.out.println(a--);
+		System.out.println(--a);
+	}
+
+}
